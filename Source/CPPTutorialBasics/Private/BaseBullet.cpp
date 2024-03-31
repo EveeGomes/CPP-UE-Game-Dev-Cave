86a569d6ab0761910244eec8df736daeed13cbaf
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2024-03-31 11:39:43 -0300

    removed comments

--- Source/CPPTutorialBasics/Private/BaseBullet.cpp
+++ Source/CPPTutorialBasics/Private/BaseBullet.cpp
@@ -10,12 +10,9 @@
 ABaseBullet::ABaseBullet()
 {
 	BulletFX = CreateDefaultSubobject<UNiagaraComponent>(TEXT("Effects"));
-	// Set the root component to be the BulletFX:
 	SetRootComponent(BulletFX);
 	CollisionSphere = CreateDefaultSubobject<USphereComponent>(TEXT("Collision Sphere"));
-	// Now, attach the collision sphere to the bullet effect sound (making the sphere a child of the soundfx)
-	CollisionSphere->SetupAttachment(BulletFX); // After adding the #include "NiagaraComponent.h" it understands that a Niagara Component is also a Scene component and stops complaining!
-
+	CollisionSphere->SetupAttachment(BulletFX);
 
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
