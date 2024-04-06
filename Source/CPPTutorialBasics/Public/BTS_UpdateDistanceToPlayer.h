3ea9a1b138544201942953fccf53451c94031dcc
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2024-04-06 15:00:03 -0300

    Solved the bullet issue with collision and added AI functionality

---
+++ Source/CPPTutorialBasics/Public/BTS_UpdateDistanceToPlayer.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTService.h"
+#include "BTS_UpdateDistanceToPlayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CPPTUTORIALBASICS_API UBTS_UpdateDistanceToPlayer : public UBTService
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/CPPTutorialBasics/Public/BTT_EnemyBasicShoot.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTT_EnemyBasicShoot.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CPPTUTORIALBASICS_API UBTT_EnemyBasicShoot : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+};
